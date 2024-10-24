f95b8cd301a9ae6128d072c071e144646834d45e
quinnelectronics <68204701+quinnelectronics@users.noreply.github.com>
2024-10-24 10:38:30 -0500

    Update fio hd testing.sh

--- fio hd testing.sh
+++ fio hd testing.sh
@@ -1,12 +1,12 @@
-#Quick Test:
+
+#Quick Test
+#sdb sdc
+
 fio --name=quicktest --ioengine=libaio --rw=randrw --rwmixread=70 --bs=4k --size=100M --direct=1 --numjobs=1 --runtime=30 --time_based --group_reporting --filename=/dev/sdb --filename=/dev/sdc --output=fio_quicktest_sdb_sdc.txt
 
 
-#Comprehensive test:
+#Comprehensive Test
+#sdb sdc
 
-#sda only
-fio --name=comprehensive_test --ioengine=libaio --rw=randrw --rwmixread=70 --bs=4k --size=10G --direct=1 --numjobs=4 --runtime=300 --time_based --group_reporting --filename=/dev/sda --output=fio_results.txt
-
-#sdb only
 fio --name=comprehensive_test --ioengine=libaio --rw=randrw --rwmixread=70 --bs=4k --size=10G --direct=1 --numjobs=4 --runtime=300 --time_based --group_reporting --filename=/dev/sdb --filename=/dev/sdc --output=fio_results_sdb_sdc.txt
 
